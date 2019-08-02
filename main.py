2541cf884b326047c5c94b1cbd9166e1d7912375
Kevin Claytor <KE.Claytor@gmail.com>
2019-08-01 21:47:37 -0400

    Logging values
    Logging all values.
    Improved display with battery and shake information.

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
-# Simple GPS module demonstration.
-# Will wait for a fix and print a message every second with the current location
-# and other details.
+
+import os
 import time
+
 import board
 import busio
 import storage
-
+import analogio
 import digitalio
+
 import adafruit_gps
 import adafruit_sdcard
 import adafruit_lis3dh
 import adafruit_framebuf
 import adafruit_is31fl3731
+
+# Battery pin
+vbat_pin = analogio.AnalogIn(board.VOLTAGE_MONITOR)
+
+def get_voltage():
+    return (vbat_pin.value * 3.3) / 65536 * 2
 
 # Configure LIS3DH Accelerometer
 i2c = busio.I2C(board.SCL, board.SDA)
@@ -31,49 +38,7 @@ storage.mount(vfs, "/sd")
 # Configure DisplayBonnet
 display = adafruit_is31fl3731.CharlieWing(i2c)
 
-#while True:
-#    if lis3dh.shake(shake_threshold=12):
-#        print("Shaken!")
-
-class scrollingDisplay():
-    def __init__(self):
-        self.d = [0]*12
-        self.f = 0
-
-    def update(self, v):
-        self.d.pop(0)
-        value = 0 if v < 0 else v
-        self.d.append(value + 1)
-        print(self.d)
-        # Update the frame
-        frame = self.f
-        display.frame(frame, show=False)
-        display.fill(0)
-        for ii in range(12):
-            value = self.d[ii]
-            for jj in range(value+1):
-                x = ii
-                y = display.height - jj
-                display.pixel(x, y, 20)
-        display.frame(frame, show=True)
-        frame = 0 if frame else 1
-        self.f = frame
-
-sd = scrollingDisplay()
-while True:
-    x, y, z = lis3dh.acceleration
-    a_mag = (x**2 + y**2 + z**2)**0.5
-    G = a_mag / adafruit_lis3dh.STANDARD_GRAVITY
-    # print(G)
-    Gpx = int((G - 1)*6/1)
-    print(G, Gpx)
-    sd.update(Gpx)
-    # print("writing")
-    # with open("/sd/test.txt", "a") as f:
-    #     f.write("%0.1f\n" % Gpx)
-    # print("done")
-    # Small delay to keep things responsive but give time for interrupt processing.
-    time.sleep(0.1)
+# Configure GPS
 
 # Define RX and TX pins for the board's serial port connected to the GPS.
 # These are the defaults you should use for the GPS FeatherWing.
@@ -84,12 +49,6 @@ TX = board.TX
 # Create a serial connection for the GPS connection using default speed and
 # a slightly higher timeout (GPS modules typically update once a second).
 uart = busio.UART(TX, RX, baudrate=9600, timeout=30)
-
-# for a computer, use the pyserial library for uart access
-#import serial
-#uart = serial.Serial("/dev/ttyUSB0", baudrate=9600, timeout=3000)
-
-# Create a GPS module instance.
 gps = adafruit_gps.GPS(uart, debug=False)
 
 # Initialize the GPS module by changing what data it sends and at what rate.
@@ -97,65 +56,172 @@ gps = adafruit_gps.GPS(uart, debug=False)
 # PMTK_220_SET_NMEA_UPDATERATE but you can send anything from here to adjust
 # the GPS module behavior:
 #   https://cdn-shop.adafruit.com/datasheets/PMTK_A11.pdf
-
 # Turn on the basic GGA and RMC info (what you typically want)
 gps.send_command(b'PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
-# Turn on just minimum info (RMC only, location):
-#gps.send_command(b'PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
-# Turn off everything:
-#gps.send_command(b'PMTK314,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
-# Tuen on everything (not all of it is parsed!)
-#gps.send_command(b'PMTK314,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0')
-
 # Set update rate to once a second (1hz) which is what you typically want.
 gps.send_command(b'PMTK220,1000')
-# Or decrease to once every two seconds by doubling the millisecond value.
-# Be sure to also increase your UART timeout above!
-#gps.send_command(b'PMTK220,2000')
-# You can also speed up the rate, but don't go too fast or else you can lose
-# data during parsing.  This would be twice a second (2hz, 500ms delay):
-#gps.send_command(b'PMTK220,500')
 
-# Main loop runs forever printing the location, etc. every second.
-last_print = time.monotonic()
+##############
+#### MAIN ####
+##############
+
+class scrollingDisplay():
+    """ Main class for storing values and updating the display
+    """
+    def __init__(self):
+        self.dg = [0]*12
+        self.ds = [0]*12
+        self.f = 0
+
+    def update_g(self, g):
+        self.dg.pop(0)
+        gpx = int((g - 1)*4/1)
+        if gpx > 4:
+            gpx = 4
+        elif gpx < 0:
+            gpx = 0
+        self.dg.append(gpx + 1)
+
+    def update_s(self, s):
+        self.ds.pop(0)
+        self.ds.append(s)
+
+    def update_b(self, b):
+        db = int((b-3.3)/3.7 * 7)
+        self.b = db
+
+    def draw_frame(self):
+        ''' Update the display
+        Show: current g's, shaken, and battery %
+        '''
+        frame = self.f
+        display.frame(frame, show=False)
+        display.fill(0)
+        for ii in range(12):
+            # Display the acceleration
+            value = self.dg[ii]
+            for jj in range(value+1):
+                x = ii
+                y = display.height - jj
+                display.pixel(x, y, 20)
+
+            # Display the shake
+            if self.ds[ii]:
+                display.pixel(x, 0, 50)
+
+        # Display the battery fraction
+        for jj in range(self.b):
+            x = 14
+            y = display.height - jj
+            display.pixel(x, y, 20)
+
+        # Show the new frame
+        display.frame(frame, show=True)
+        frame = 0 if frame else 1
+        self.f = frame
+
+sd = scrollingDisplay()
+# Find the number of files in the directory
+# nfiles = len(os.listdir("/sd"))
+# filename = "/sd/data_{}.txt".format(nfiles+1)
+
+# Wait until we have a fix
 while True:
-    # Make sure to call gps.update() every loop iteration and at least twice
-    # as fast as data comes from the GPS unit (usually every second).
-    # This returns a bool that's true if it parsed new data (you can ignore it
-    # though if you don't care and instead look at the has_fix property).
     gps.update()
-    # Every second print out current location details if there's a fix.
-    current = time.monotonic()
-    if current - last_print >= 1.0:
-        last_print = current
+    if not gps.has_fix:
+        # Try again if we don't have a fix yet.
+        print('Waiting for fix...')
+        time.sleep(1)
+        continue
+    else:
+        print("Fix acquired.")
+        break
+
+# Use the date as a filename
+filename = "{}-{}-{}T{:02}:{:02}".format(
+            gps.timestamp_utc.tm_year,
+            gps.timestamp_utc.tm_mon,
+            gps.timestamp_utc.tm_mday,
+            gps.timestamp_utc.tm_hour,
+            gps.timestamp_utc.tm_min
+            )
+
+# Only log for a specified duration
+duration = 10
+start_time = time.monotonic()
+with open(filename, "w") as f:
+    # Write header
+    print("Logging...")
+    f.write("time,battery,g,shaken,latitude,longitude,quality,satellites,altitude,speed,track_angle,horizontal_dilution\n")
+    while True:
+        # Wait until we have a fix
+        gps.update()
+
         if not gps.has_fix:
             # Try again if we don't have a fix yet.
-            print('Waiting for fix...')
+            print('Lost fix, re-waiting for fix...')
+            time.sleep(1)
             continue
+
         # We have a fix! (gps.has_fix is true)
-        # Print out details about the fix like location, date, etc.
-        print('=' * 40)  # Print a separator line.
-        print('Fix timestamp: {}/{}/{} {:02}:{:02}:{:02}'.format(
-            gps.timestamp_utc.tm_mon,   # Grab parts of the time from the
-            gps.timestamp_utc.tm_mday,  # struct_time object that holds
-            gps.timestamp_utc.tm_year,  # the fix time.  Note you might
+        time_str = "{}/{}/{} {:02}:{:02}:{:02}".format(
+            gps.timestamp_utc.tm_year,  # Grab parts of the time from the
+            gps.timestamp_utc.tm_mon,   # struct_time object that holds
+            gps.timestamp_utc.tm_mday,  # the fix time.  Note you might
             gps.timestamp_utc.tm_hour,  # not get all data like year, day,
             gps.timestamp_utc.tm_min,   # month!
-            gps.timestamp_utc.tm_sec))
-        print('Latitude: {0:.6f} degrees'.format(gps.latitude))
-        print('Longitude: {0:.6f} degrees'.format(gps.longitude))
-        print('Fix quality: {}'.format(gps.fix_quality))
-        # Some attributes beyond latitude, longitude and timestamp are optional
-        # and might not be present.  Check if they're None before trying to use!
+            gps.timestamp_utc.tm_sec)
+
+        # Get battery voltage
+        v_batt = get_voltage()
+
+        # Now get acceleration
+        x, y, z = lis3dh.acceleration
+        a_mag = (x**2 + y**2 + z**2)**0.5
+        G = a_mag / adafruit_lis3dh.STANDARD_GRAVITY
+        shaken = lis3dh.shake(shake_threshold=12)
+        sd.update_g(G)
+        sd.update_s(shaken)
+        sd.update_b(v_batt)
+
+        # Update the display
+        sd.draw_frame()
+
+        # Start writing to the file
+        f.write("%s,%f,%f,%d,%f,%f,%f," %
+            (
+                time_str, v_batt, G, shaken,
+                gps.latitude, gps.longitude, gps.fix_quality,
+            )
+        )
         if gps.satellites is not None:
-            print('# satellites: {}'.format(gps.satellites))
+            f.write("%d," % gps.satellites)
+        else:
+            f.write("None,")
         if gps.altitude_m is not None:
-            print('Altitude: {} meters'.format(gps.altitude_m))
+            f.write("%f," % gps.altitude_m)
+        else:
+            f.write("None,")
         if gps.speed_knots is not None:
-            print('Speed: {} knots'.format(gps.speed_knots))
+            f.write("%f," % gps.speed_knots)
+        else:
+            f.write("None,")
         if gps.track_angle_deg is not None:
-            print('Track angle: {} degrees'.format(gps.track_angle_deg))
+            f.write("%f," % gps.track_angle_deg)
+        else:
+            f.write("None,")
         if gps.horizontal_dilution is not None:
-            print('Horizontal dilution: {}'.format(gps.horizontal_dilution))
-        if gps.height_geoid is not None:
-            print('Height geo ID: {} meters'.format(gps.height_geoid))
+            f.write("%f" % gps.horizontal_dilution)
+        else:
+            f.write("None")
+        f.write("\n")
+
+        # Write to SD card
+        # Small delay to keep things responsive but give time for interrupt processing.
+        time.sleep(0.1)
+
+        # If we have recorded long enough, stop
+        if (t - start_time) > duration:
+            break
+
+print("Finished.")
